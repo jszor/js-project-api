fbd47b548dc8b9fddb05458a2657fca541c71c23
Juan Salvador Zorrilla <jsalvadorzorrilla@gmail.com>
2025-06-11 22:02:07 +0200

    modify thought schema

--- models/thought.js
+++ models/thought.js
@@ -1,11 +1,21 @@
 import mongoose from "mongoose"
 
 const thoughtSchema = new mongoose.Schema({
-  message: { type: String, required: true },
-  hearts: { type: Number, default: 0 },
-}, 
-{ 
-  timestamps: { createdAt: true, updatedAt: false }
+  message: { 
+    type: String, 
+    required: true,
+    minLength: 5,
+    maxLength: 140
+  },
+  hearts: { 
+    type: Number, 
+    default: 0,
+    min: 0
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
 })
 
 export const Thought = mongoose.model("Thought", thoughtSchema)
